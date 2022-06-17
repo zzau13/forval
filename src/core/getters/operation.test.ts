d1c8b62cb55e62387127433fb30407eb20630f23
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-06-17 22:46:00 +0200

    fix: unit test

--- src/core/getters/operation.test.ts
+++ src/core/getters/operation.test.ts
@@ -1,17 +1,22 @@
+import { it, describe, expect } from 'vitest';
 import { OperationObject } from 'openapi3-ts';
+
 import { getOperationId } from './operation';
 
 describe('getOperationId getter', () => {
   [
-    ['/api/test/{id}', 'ApiTestId'],
-    ['/api/test/{user_id}', 'ApiTestUserId'],
-    ['/api/test/{locale}.js', 'ApiTestLocaleJs'],
-    ['/api/test/i18n-{locale}.js', 'ApiTestI18nLocaleJs'],
-    ['/api/test/{param1}-{param2}.js', 'ApiTestParam1Param2Js'],
-    ['/api/test/user{param1}-{param2}.html', 'ApiTestUserparam1Param2Html'],
+    ['/api/test/{id}', 'PostApiTestId'],
+    ['/api/test/{user_id}', 'PostApiTestUserId'],
+    ['/api/test/{locale}.js', 'PostApiTestLocaleJs'],
+    ['/api/test/i18n-{locale}.js', 'PostApiTestI18nLocaleJs'],
+    ['/api/test/{param1}-{param2}.js', 'PostApiTestParam1Param2Js'],
+    // TODO: what
+    ['/api/test/user{param1}-{param2}.html', 'PostApiTestUserparam1Param2Html'],
   ].forEach(([input, expected]) => {
     it(`should process ${input} to ${expected}`, () => {
-      expect(getOperationId({} as OperationObject, input, '')).toBe(expected);
+      expect(getOperationId({} as OperationObject, input, 'post')).toBe(
+        expected,
+      );
     });
   });
 });
--- src/core/getters/route.test.ts
+++ src/core/getters/route.test.ts
@@ -1,3 +1,4 @@
+import { it, describe, expect } from 'vitest';
 import { getRoute } from './route';
 
 describe('getRoute getter', () => {
